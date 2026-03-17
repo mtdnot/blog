563a145d0757cbf3d9508cd157090bc7e55835fe
mtdnot <mtdnot1129@gmail.com>
2026-03-17 02:41:01 +0000

    Switch to GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://blog.mtdnot.dev',
+  site: 'https://mtdnot.github.io',
+  base: '/blog',
   output: 'static',
   build: {
     format: 'directory'
