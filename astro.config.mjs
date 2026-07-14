6f31fcf0beaf2e8ec247a9688fe45be6229f34a6
mtdnot <mtdnot1129@gmail.com>
2026-07-14 16:55:10 +0900

    Update header branding layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ export default defineConfig({
   site: 'https://blog.mtdnot.dev',
   base: '/',
   output: 'static',
+  devToolbar: {
+    enabled: false
+  },
   build: {
     format: 'directory'
   },
